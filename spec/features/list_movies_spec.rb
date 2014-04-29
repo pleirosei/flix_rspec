613fed8fef5dae74dd0d9bb139a9f189c4e6d81e
Sean Livingston <sean.d.livingston@gmail.com>
2014-04-28 22:20:49 -0400

    TDD: Added show_page

--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'Viewing the list of movies' do
 
   it "shows the movies" do
-   movie1 = Movie.create(title: "Iron Man",
+    movie1 = Movie.create(title: "Iron Man",
                           rating: "PG-13",
                           total_gross: 318412101.00,
                           description: "Tony Stark builds an armored suit to fight the throes of evil",
